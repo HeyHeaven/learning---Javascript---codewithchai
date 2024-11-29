907ef753bd3f7eb4ac59e55c506bfe8f2d151350
kunal <kunalthakur123@gmaial.com>
2024-11-29 17:03:43 +0530

    new lesson and file added

---
+++ datatypes_and_variable.js
@@ -0,0 +1,56 @@
+// new features to declare var - preferable
+let x = 56
+const y = 1;
+// old version to declare var
+var z = "kundan"
+
+// *------ datatypes - Primitive - 7 types ------*
+
+/*
+Number(z) - 49, 10.2
+Boolean(y) - true, false, 0, 1 
+String() - "kudnan", "thakur", "etc.."
+BigInt(x) -  82834752072, 19n, big scientific values and etc.
+Symbol() - 
+    x = Symbol("abc")
+    y = Symbol("abc")
+        here x and y is not equal.
+null - Object
+undefined - undefined
+*/
+
+// *------ datatypes - Non Primitive/Reference type - 3 types -----*
+
+// Array -  [8, 3,"kundan", null]
+
+// Object -
+/*
+let obj ={
+    name: kundan,
+    age: 20,
+    gender: male
+}
+*/
+
+// Functions
+
+// let func1 = function () {
+//     console.log("hello func world");
+    
+// }
+
+console.log(typeof([9,4,2]));
+
+
+// console.log(+"");  -- gives 0 output of Number type
+
+// Arthematic Operations
+
+// 2+2
+// 2-2
+// 2*2
+// 2**2 - for index power
+// 2%2 - for module
+
+
+

333a227d53358458762261265d1246679dd8aa36
kunal <kunalthakur123@gmaial.com>
2024-12-14 22:19:55 +0530

    .

--- loops.js
+++ loops.js
@@ -8,16 +8,34 @@ for (let i = 0; i <= 15; i++) { // simple syntax as usual.
 
 // 1.b. nested for loop
 for (let i = 1; i <= 5; i++) {// yes. we can use same variable for different for loops.. cuz of scooping
-    console.log(`Table of ${i}`);
+    // console.log(`Table of ${i}`);
     for (let j = 1; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i*j}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
     }
 }
 
-// while loop
-let i=0;
-while (i<=5) {
-    console.log(i);
-    
+// 1.c "for of" loop
+const arr = [2, 6, 3, 2, 623, 54]
+const str = 'kundan'
+const map1 = new Map();
+map1.set('IN','Indian')
+map1.set('DU','Dubai')
+map1.set('US','United State')
+for (const [i, j] of map1) { // here object is anything that is iterable. eg. arr, string, map is some example
+    console.log(j);
 }
+
+// while loop
+let i=0; // declaring the iterator
+while (i<=5) { // some condition to stop loop
+    // console.log(i);
+    i++;       // something to inscrement loop
+}
+
+// do .. while loop
+let j=19
+do {
+    // console.log(j);
+    j++;
+} while (j < 18);  // here, even though condition was false, it still runs the loop once.
 
